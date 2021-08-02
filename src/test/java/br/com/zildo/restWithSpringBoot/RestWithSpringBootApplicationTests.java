10afcdd1ee8431edcfa0f2983a54731f73f01639
Zildo Borcathe <zildo.borcathe_actdigital@prestador.globo>
2021-08-02 09:42:25 -0300

    Adicionado Spring Boot

---
+++ src/test/java/br/com/zildo/restWithSpringBoot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zildo.restWithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
